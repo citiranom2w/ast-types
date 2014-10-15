citiranom2w/ast-types

commit 94adb160509d35a4c03bccd49030296770737e7a

Ensure that function expressions with ids bind their id inside.
Per https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions_and_function_scope:

> (With a function expression,) The function name can be used only
> within the function's body. Attempting to use it outside the
> function's body results in an error (or undefined if the function name
> was previously declared via a var statement).

lib/scope.js
@@ -94,7 +94,12 @@ function scanScope(path, bindings) {
 function recursiveScanScope(path, bindings) {
     var node = path.value;
 
-    if (isArray.check(node)) {
+    if (path.parent &&
+        namedTypes.FunctionExpression.check(path.parent.node) &&
+        path.parent.node.id) {
+        addPattern(path.parent.get("id"), bindings);
+
+    } else if (isArray.check(node)) {
         path.each(function(childPath) {
             recursiveScanChild(childPath, bindings);
         });

test/run.js
@@ -555,21 +555,30 @@ describe("scope.getBindings", function () {
         "var foo = 42;",
         "function bar(baz) {",
         "  return baz + foo;",
-        "}"
+        "}",
+        "var nom = function rom(pom) {",
+        "  return rom(pom);",
+        "};"
     ];
 
     var ast = parse(scope.join("\n"));
     it("should get local and global scope bindings", function() {
         traverse(ast, function(node) {
+            var bindings;
             if (n.Program.check(node)) {
-                var bindings = this.scope.getBindings();
-                assert.deepEqual(["bar", "foo"], Object.keys(bindings).sort());
+                bindings = this.scope.getBindings();
+                assert.deepEqual(["bar", "foo", "nom"], Object.keys(bindings).sort());
                 assert.equal(1, bindings.foo.length);
                 assert.equal(1, bindings.bar.length);
             } else if (n.FunctionDeclaration.check(node)) {
-                var bindings = this.scope.getBindings();
+                bindings = this.scope.getBindings();
                 assert.deepEqual(["baz"], Object.keys(bindings));
                 assert.equal(1, bindings.baz.length);
+            } else if (n.ReturnStatement.check(node) &&
+                       n.Identifier.check(node.argument) &&
+                       node.argument.name === "rom") {
+                bindings = this.scope.getBindings();
+                assert.deepEqual(["pom", "rom"], Object.keys(bindings).sort());
             }
         });
     });

commit 32a24aef65a54eb4cda67525a509670da9286102

Fix named function expression scopes.
The commit that added support for function expression names counting as
part of the binding for that function’s scope,
94adb160509d35a4c03bccd49030296770737e7a, caused only the function name
to count as a binding in that function. For example:

    var f = function f(a, b) { var c; }

Inside `f`, the only bound name was `f` itself referring to the
function expression. It should have bound `a`, `b`, `c`, and `f`.

lib/scope.js
@@ -139,8 +139,9 @@ function recursiveScanScope(path, bindings) {
         namedTypes.FunctionExpression.check(path.parent.node) &&
         path.parent.node.id) {
         addPattern(path.parent.get("id"), bindings);
+    }
 
-    } else if (!node) {
+    if (!node) {
         // None of the remaining cases matter if node is falsy.
 
     } else if (isArray.check(node)) {

test/run.js
@@ -735,6 +735,7 @@ describe("scope methods", function () {
         "  return baz + foo;",
         "}",
         "var nom = function rom(pom) {",
+        "  var zom;",
         "  return rom(pom);",
         "};"
     ];
@@ -760,7 +761,8 @@ describe("scope methods", function () {
                        n.CallExpression.check(node.argument) &&
                        node.argument.callee.name === "rom") {
                 bindings = this.scope.getBindings();
-                assert.deepEqual(["pom", "rom"], Object.keys(bindings).sort());
+                assert.deepEqual(["pom", "rom", "zom"], Object.keys(bindings).sort());
+                checked.push(node);
             }
         });
 